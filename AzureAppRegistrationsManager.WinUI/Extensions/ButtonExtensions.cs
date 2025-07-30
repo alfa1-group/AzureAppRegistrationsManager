c924d17051e28bf65578c161bed4c27e6388f2ea
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-07-30 09:28:23 +0200

    copy

---
+++ AzureAppRegistrationsManager.WinUI/Extensions/ButtonExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.UI.Xaml.Controls;
+
+internal static class ButtonExtensions
+{
+    internal static List<TextBox> GetChildTextBoxes(this Button button)
+    {
+        if (button.Parent is Panel panel)
+        {
+            return panel.Children.OfType<TextBox>().ToList();
+        }
+
+        return [];
+    }
+}
